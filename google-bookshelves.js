dc451b554839cb4ade9f61862449d8f410ba976c
Adam Harris <aharris88@users.noreply.github.com>
2014-06-01 20:03:58 -0600

    Add initialize method

--- google-bookshelves.js
+++ google-bookshelves.js
@@ -1,17 +1,34 @@
 var googleBookshelves = (function() {
-  var showShelf = function(options) {
-    options.idNumber = options.idNumber || "113720634485746776434";
-    options.shelf = options.shelf || "Reading Now";
-    options.container = options.container || "#readingNow";
-    options.limit = options.limit || 10;
-    options.random = (typeof options.random !== 'undefined')? options.random: false;
-    options.pageCurl = (typeof options.pageCurl !== 'undefined')? options.pageCurl: false;
-    options.imageSize = options.imageSize || "thumb";
+  var idNumber;
+  var shelf;
+  var container;
+  var limit;
+  var random;
+  var pageCurl;
+  var imageSize;
 
-    return options;
+  var showShelf = function(options) {
+    initialize(options);
+    $(container).html(shelf);
   };
 
-  return {
+  var initialize = function(options) {
+    idNumber = options.idNumber || "113720634485746776434";
+    shelf = options.shelf || "Reading Now";
+    container = options.container || "#readingNow";
+    limit = options.limit || 10;
+    random = (typeof options.random !== 'undefined')? options.random: false;
+    pageCurl = (typeof options.pageCurl !== 'undefined')? options.pageCurl: false;
+    imageSize = options.imageSize || "thumb";
+  };
+
+  var public = {
     showShelf: showShelf
   };
+
+  public._private = {
+    initialize: initialize
+  };
+
+  return public;
 }());
